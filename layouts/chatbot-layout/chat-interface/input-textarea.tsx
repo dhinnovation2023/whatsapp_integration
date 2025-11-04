2087384241d4e6b52fc5d4f00dffd65c24edf2ae
vishnu <iam.vishnu.bhaskar@gmail.com>
2025-11-04 08:34:20 +0530

    Enhance input textarea component; add file attachment functionality and improve UI for selected attachments

--- layouts/chatbot-layout/chat-interface/input-textarea.tsx
+++ layouts/chatbot-layout/chat-interface/input-textarea.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { RiLoader4Line, RiSendPlaneLine } from '@remixicon/react'
-import { useState } from 'react'
+import { RiAddLargeLine, RiCloseLine, RiLoader4Line, RiSendPlaneLine } from '@remixicon/react'
+import { useRef, useState } from 'react'
 
 const InputTextarea = ({
     onSubmit,
@@ -12,15 +12,68 @@ const InputTextarea = ({
 }) => {
 
     const [value, setValue] = useState<string>('');
+    const [attachment, setAttachment] = useState<File | null>(null);
+
+    const fileInputElement = useRef<HTMLInputElement>(null);
 
     return (
         <div
-            className='py-3 px-4 bg-background flex items-center rounded-2xl'
+            className='py-2 px-5 bg-background flex items-center rounded-2xl gap-3 relative'
         >
+            {
+                attachment && (
+                    <div
+                        className='absolute bottom-[70px] left-0 bg-background shadow-2xl py-3 px-4 rounded-2xl flex items-center gap-5'
+                    >
+                        <div>
+                            <p
+                                className='text-xs font-semibold text-foreground/60'
+                            >Selected attachment:</p>
+                            <p
+                                className='text-lg font-bold'
+                            >{attachment.name}</p>
+                        </div>
+                        <button
+                            className='shrink-0 cursor-pointer'
+                            onClick={() => {
+                                setAttachment(null)
+                            }}
+                        >
+                            <RiCloseLine
+                                size={20}
+                            />
+                        </button>
+                    </div>
+                )
+            }
+            <>
+                <button
+                    className='shrink-0 cursor-pointer'
+                    onClick={() => {
+                        if (fileInputElement.current) {
+                            fileInputElement.current.click();
+                        }
+                    }}
+                >
+                    <RiAddLargeLine
+                        size={20}
+                    />
+                </button>
+                <input
+                    type="file"
+                    hidden
+                    ref={fileInputElement}
+                    onChange={(event) => {
+                        if (event.target.files?.[0]) {
+                            setAttachment(event.target.files[0]);
+                        }
+                    }}
+                />
+            </>
             <input
                 type="text"
                 placeholder='Enter message'
-                className='w-full pl-[17px] outline-none'
+                className='w-full outline-none'
                 value={value}
                 onChange={(event) => setValue(event.target.value)}
                 onKeyDown={(event) => {
@@ -31,7 +84,7 @@ const InputTextarea = ({
                 }}
             />
             <button
-                className='shrink-0 rounded-md bg-theme-primary text-white min-w-[50px] h-[50px] flex items-center justify-center cursor-pointer'
+                className='shrink-0 rounded-2xl bg-theme-primary text-white min-w-[50px] h-[50px] flex items-center justify-center cursor-pointer'
                 onClick={() => {
                     onSubmit(value);
                     setValue("")
