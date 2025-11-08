3c30ae1585e4c6c485b7f78222116927ba1cf413
vishnu <iam.vishnu.bhaskar@gmail.com>
2025-11-08 10:11:38 +0530

    Restrict normal user to assign team members

--- app/api/whatsapp/update-assigned/route.ts
+++ app/api/whatsapp/update-assigned/route.ts
@@ -1,9 +1,21 @@
 import { handleCatchBlock } from "@/functions/common";
 import { updateAssigned } from "@/functions/whatsapp/update-assigned";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
     try {
+
+        const SUPER_ADMIN_EMAIL = process.env.SUPER_ADMIN_EMAIL;
+
+        if (!SUPER_ADMIN_EMAIL) {
+            throw new Error("Please provide SUPER_ADMIN_EMAIL in env file.")
+        }
+
+        const session = await getServerSession();
+        if (!session?.user?.email || session.user.email !== SUPER_ADMIN_EMAIL) {
+            throw new Error("User is not authorized!");
+        }
 
         const body = await request.json() as {
             userId: string,
