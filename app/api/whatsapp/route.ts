8be0c5754e8031816792a6dc4644666883c50906
vishnu <iam.vishnu.bhaskar@gmail.com>
2025-11-10 16:52:12 +0530

    bug fix: image with caption not recieving. - try 1

--- app/api/whatsapp/route.ts
+++ app/api/whatsapp/route.ts
@@ -35,6 +35,8 @@ export async function POST(req: NextRequest) {
             const from = m.from;
             const timestamp = m.timestamp;
 
+            console.log("Message object:", m)
+
             // save to database
 
             await dbConnect();
