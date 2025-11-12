41e18e597d304c865139e3d0f1f8e6b3cf0347f9
vishnu <iam.vishnu.bhaskar@gmail.com>
2025-11-12 09:13:09 +0530

    Load more contact automatically when the load more button visible in viewport;

--- layouts/chatbot-layout/chat-sidebar/index.tsx
+++ layouts/chatbot-layout/chat-sidebar/index.tsx
@@ -4,7 +4,7 @@ import ErrorTemplate from '@/components/ui-elements/error-template';
 import { handleCatchBlock } from '@/functions/common';
 import { RiArrowDownSLine, RiLoader4Line, RiSearchLine } from '@remixicon/react'
 import axios from 'axios';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import ContactCard from './contact-card';
 import { useSearchParams } from 'next/navigation';
 import { CustomContactsCardDataInterface } from '@/app/api/whatsapp/fetch-contacts/all/route';
@@ -25,6 +25,7 @@ const ChatSidebar = () => {
     // Pagination
     const [currentPage, setCurrentPage] = useState<number>(1);
     const [paginationLoading, setPaginationLoading] = useState<boolean>(false);
+    const loadMoreElement = useRef<HTMLButtonElement>(null)
 
     const searchParams = useSearchParams();
     const [isHidden, setIsHidden] = useState(false);
@@ -34,6 +35,22 @@ const ChatSidebar = () => {
         const isMobile = phone && window.innerWidth < 500 ? true : false;
         return isMobile;
     }, [searchParams])
+
+    useEffect(() => {
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting ) {
+                    setCurrentPage(prev => ++prev)
+                }
+            });
+        })
+
+        if (loadMoreElement.current) {
+            observer.observe(loadMoreElement.current);
+        }
+
+    }, [isLoading])
 
     useEffect(() => {
 
@@ -177,6 +194,7 @@ const ChatSidebar = () => {
                                     className='flex items-center justify-center py-3 px-4 gap-2 text-theme-primary text-center w-full cursor-pointer font-semibold hover:bg-theme-primary/10 transition-all rounded-xl'
                                     disabled={paginationLoading}
                                     onClick={() => setCurrentPage(prev => ++prev)}
+                                    ref={loadMoreElement}
                                 >
                                     {
                                         paginationLoading ? (
@@ -186,12 +204,12 @@ const ChatSidebar = () => {
                                             />
                                         ) : (
                                             <RiArrowDownSLine
-                                        size={25}
-                                        className='shrink-0'
-                                    />
+                                                size={25}
+                                                className='shrink-0'
+                                            />
                                         )
                                     }
-                                    <span>Load more</span>
+                                    <span>{paginationLoading ? "Loading Contacts..." : "Load more"}</span>
                                 </button>
                             </div>
                         </div>
