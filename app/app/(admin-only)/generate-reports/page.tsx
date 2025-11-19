7b5822b473aae0e7381e494d14eb01b5ea75bb33
vishnu <iam.vishnu.bhaskar@gmail.com>
2025-11-19 15:43:26 +0530

    Add generate reports functionality and team management features

---
+++ app/app/(admin-only)/generate-reports/page.tsx
@@ -0,0 +1,15 @@
+import DashboardLayout from '@/layouts/dashboard'
+import GenerateReportPageClientComponent from './client-component'
+
+const ReportsPage = async () => {
+
+  return (
+    <DashboardLayout
+      pageTitle='Generate Reports'
+    >
+      <GenerateReportPageClientComponent />
+    </DashboardLayout>
+  )
+}
+
+export default ReportsPage
---
+++ app/app/(admin-only)/layout.tsx
@@ -0,0 +1,41 @@
+import ErrorTemplate from "@/components/ui-elements/error-template";
+import { handleCatchBlock } from "@/functions/common";
+import DashboardLayout from "@/layouts/dashboard";
+import { getServerSession } from "next-auth";
+import { PropsWithChildren } from "react";
+
+export default async function AdminOnlyPagesLayout({ children }: PropsWithChildren) {
+
+    try {
+        const session = await getServerSession();
+        const SUPER_ADMIN_EMAIL = process.env.SUPER_ADMIN_EMAIL;
+
+        if (!SUPER_ADMIN_EMAIL) {
+            throw new Error("Please provide SUPER_ADMIN_EMAIL in .env");
+        }
+
+        if (session?.user?.email !== SUPER_ADMIN_EMAIL) {
+            throw new Error("You are not authorized to access here.")
+        }
+
+    } catch (err) {
+        const message = handleCatchBlock(err);
+        return (
+            <DashboardLayout
+                pageTitle="Only Super Admin"
+            >
+                <ErrorTemplate
+                    error={message}
+                />
+            </DashboardLayout>
+        )
+    }
+
+    console.log("Testing 1")
+
+    return (
+        <>
+            {children}
+        </>
+    )
+}
