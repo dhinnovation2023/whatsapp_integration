f6c6d69310aeabf539f11d0d02f2138deeba066e
vishnu <iam.vishnu.bhaskar@gmail.com>
2025-11-19 15:15:04 +0530

    restrict generate-report and oauth page for normal users

--- app/app/generate-reports/page.tsx
+++ app/app/generate-reports/page.tsx
@@ -1,13 +1,41 @@
 import DashboardLayout from '@/layouts/dashboard'
 import GenerateReportPageClientComponent from './client-component'
+import { getServerSession } from 'next-auth'
+import { handleCatchBlock } from '@/functions/common';
+import ErrorTemplate from '@/components/ui-elements/error-template';
 
 const ReportsPage = async () => {
 
+  try {
+    const session = await getServerSession();
+    const SUPER_ADMIN_EMAIL = process.env.SUPER_ADMIN_EMAIL;
+
+    if (!SUPER_ADMIN_EMAIL) {
+      throw new Error("Please provide SUPER_ADMIN_EMAIL in .env");
+    }
+
+    if (session?.user?.email !== SUPER_ADMIN_EMAIL) {
+      throw new Error("you are not authorized to access here.")
+    }
+
+  } catch (err) {
+    const message = handleCatchBlock(err);
+    return (
+      <DashboardLayout
+        pageTitle='Generate Report'
+      >
+        <ErrorTemplate
+          error={message}
+        />
+      </DashboardLayout>
+    )
+  }
+
   return (
     <DashboardLayout
-        pageTitle='Generate Reports'
+      pageTitle='Generate Reports'
     >
-      <GenerateReportPageClientComponent/>
+      <GenerateReportPageClientComponent />
     </DashboardLayout>
   )
 }
--- app/app/google-oauth/page.tsx
+++ app/app/google-oauth/page.tsx
@@ -1,7 +1,36 @@
+import ErrorTemplate from "@/components/ui-elements/error-template"
+import { handleCatchBlock } from "@/functions/common"
 import { generateAuthUrl } from "@/functions/google-auth/googleOAuth"
+import DashboardLayout from "@/layouts/dashboard"
+import { getServerSession } from "next-auth"
 import { redirect } from "next/navigation"
 
 const page = async () => {
+
+    try {
+        const session = await getServerSession();
+        const SUPER_ADMIN_EMAIL = process.env.SUPER_ADMIN_EMAIL;
+
+        if (!SUPER_ADMIN_EMAIL) {
+            throw new Error("Please provide SUPER_ADMIN_EMAIL in .env");
+        }
+
+        if (session?.user?.email !== SUPER_ADMIN_EMAIL) {
+            throw new Error("you are not authorized to access here.")
+        }
+
+    } catch (err) {
+        const message = handleCatchBlock(err);
+        return (
+            <DashboardLayout
+                pageTitle="Google OAuth Page"
+            >
+                <ErrorTemplate
+                    error={message}
+                />
+            </DashboardLayout>
+        )
+    }
 
     const OAUthUrl = generateAuthUrl({
         scopes: [
