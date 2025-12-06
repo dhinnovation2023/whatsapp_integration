b734479a9bd5e604524708ad893bb5ac3c317906
vishnu <iam.vishnu.bhaskar@gmail.com>
2025-12-06 16:31:20 +0530

    Same error bug fix try 3;

--- layouts/dashboard/sidebar/index.tsx
+++ layouts/dashboard/sidebar/index.tsx
@@ -16,15 +16,10 @@ const DashboardSidebar = () => {
   }, [deviceType])
 
   return (
-    <div
-      className={
-        `${openSidebar ? "max-w-[300px] py-6 px-4" : "max-w-0 py-6"} w-full transition-all md:transition-none shrink-0 overflow-y-auto bg-background min-h-dvh max-h-dvh md:min-h-[300px] shadow-lg shadow-neutral-200 z-40`
-        + ` fixed md:relative`
-      }
-    >
+    <>
 
       <button
-        className={`fixed cursor-pointer transition-all md:transition-none ${openSidebar ? "left-[300px]" : "left-0"} bottom-10 bg-foreground text-white border border-stroke-light border-l-0 py-2 px-2 rounded-tr-2xl rounded-br-2xl`}
+        className={`fixed cursor-pointer transition-all md:transition-none z-50 ${openSidebar ? "left-[300px]" : "left-0"} bottom-10 bg-foreground text-white border border-stroke-light border-l-0 py-2 px-2 rounded-tr-2xl rounded-br-2xl`}
         onClick={() => setOpenSidebar(prev => !prev)}
       >
         {
@@ -41,12 +36,20 @@ const DashboardSidebar = () => {
       </button>
 
       <div
-        className='space-y-0 min-w-max'
+        className={
+          `${openSidebar ? "max-w-[300px] py-6 px-4" : "max-w-0 py-6"} w-full transition-all md:transition-none shrink-0 overflow-y-auto bg-background min-h-dvh max-h-dvh md:min-h-[300px] shadow-lg shadow-neutral-200 z-40`
+          + ` fixed md:relative`
+        }
       >
-        <UserInfo />
-        <SidebarMenu />
+
+        <div
+          className='space-y-0 min-w-max'
+        >
+          <UserInfo />
+          <SidebarMenu />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
