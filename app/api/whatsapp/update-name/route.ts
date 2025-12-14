73d55f8c9ccc8efffdafc0e98cbc90cbe921af3a
vishnu <iam.vishnu.bhaskar@gmail.com>
2025-12-14 09:20:27 +0530

    Bug fix: Saving contact without name issue

--- app/api/whatsapp/update-name/route.ts
+++ app/api/whatsapp/update-name/route.ts
@@ -6,6 +6,11 @@ export async function POST(request: NextRequest) {
     try {
 
         const body = await request.json() as UpdateContactNameRequestData;
+
+        if (!body.newName) {
+            throw new Error("Name field is required.")
+        }
+
         await updateContactName(body);
 
         return NextResponse.json(true);
